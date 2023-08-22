fc482787cd9f9624e947a449d8c5dc00dca9a268
Hayden <hayden.l.phillips@outlook.com>
2023-08-22 09:24:00 -0500

    fix errors

--- pages/brother.jsx
+++ pages/brother.jsx
@@ -1,48 +1,40 @@
-import{useEffect, useState} from "react";
-import "./Brother.js";
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Brother(){
-    const [Brother, setBrother] = useState([])
-    useEffect(()=> {
+function Brother() {
+    const [brothersheet, setBrothersheet] = useState([]);
+
+    useEffect(() => {
         axios.get('http://localhost:3001/getBrothersheet')
-        .them(Brother =>  setUsers(Brother.data))
-        .catch(err => console.log(err))
-    }, [] )
+            .then(response => setBrothersheet(response.data))
+            .catch(err => console.log(err));
+    }, []);
 
     return (
         <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
-        <div className="w-50"></div>
-        <table classname="table"> 
-            <thead>
-                <tr>
-                    <th>
-                        item
-                    </th>
-                    <th>
-                        price
-                    </th>
-                    <th>
-                        availability
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-              {
-                Brother.map(Brother =>{
-                    return <tr>
-                        <tb>{Brother.item}</tb>
-                        <tb>{Brother.price}</tb>
-                        <tb>{Brother.avaikability}</tb>
-                    </tr>
-                })
-              }
-            </tbody>
-        </table>
-
+            <div className="w-50">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Item</th>
+                            <th>Price</th>
+                            <th>Availability</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {brothersheet.map(brother => (
+                            <tr key={brother._id}>
+                                <td>{brother.item}</td>
+                                <td>{brother.price}</td>
+                                <td>{brother.availability}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-);
+    );
 }
+
 export default Brother;
